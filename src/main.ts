4cfda8c48e0b71573f35826c340e3914b7932da9
Zilba26 <basile.lebaudy@gmail.com>
2024-01-09 22:46:00 +0100

    front finished

--- src/main.ts
+++ src/main.ts
@@ -1,131 +1,61 @@
 import "./style.css";
-import L from "leaflet";
+import L, { Map } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { main, predictForRoute } from "./prediction";
-import "leaflet-routing-machine";
+import { Sequential } from "@tensorflow/tfjs";
 
-const map = L.map("map");
+let model: Sequential;
+let map: Map;
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+main().then((modelResult: any) => {
+  console.log("model: " + modelResult);
+  model = modelResult;
+  document.getElementById("map")!.innerHTML = "";
 
-const roadList = [
-  {
-    "coordinates": [
-      [
-        -1.583123434893785,
-        47.23518597380839
-      ],
-      [
-        -1.57863384902866,
-        47.23310863996006
-      ]
-    ],
-    "color": "red"
-  }, {
-    "coordinates": [
-      [
-        -1.568160939861016,
-        47.23568134656463
-      ],
-      [
-        -1.566080140559598,
-        47.23328459825937
-      ]
-    ],
-    "color": "orange"
-  },
-  {
-    "coordinates": [
-      [
-        -1.56156574731029,
-        47.210028262117945
-      ],
-      [
-        -1.562539243278916,
-        47.21027496416676
-      ],
-      [
-        -1.563191790976123,
-        47.21029415682456
-      ],
-      [
-        -1.566800486579925,
-        47.20923708671452
-      ]
-    ],
-    "color": "green"
-  },
-  {
-    "coordinates": [
-      [
-        -1.552294562802103,
-        47.23466572372188
-      ],
-      [
-        -1.551469053128831,
-        47.23498042534494
-      ],
-      [
-        -1.55085236056989,
-        47.23512504539897
-      ],
-      [
-        -1.549423097575776,
-        47.23532491490086
-      ]
-    ],
-    "color": "green"
-  },
-  {
-    "coordinates": [
-      [
-        -1.564411039393482,
-        47.2156145242085
-      ],
-      [
-        -1.563747324370985,
-        47.2152768643552
-      ]
-    ],
-    "color": "green"
-  },
-  {
-    "coordinates": [
-      [
-        -1.564411039393482,
-        47.2156145242085
-      ],
-      [
-        -1.563747324370985,
-        47.2152768643552
-      ]
-    ],
-  }
-];
-
-
-roadList.forEach(road => {
-  // L.polyline(road.coordinates.map(coordinate => L.latLng(coordinate[1], coordinate[0])), { color: road.color, opacity: 1, weight: 5 }).addTo(map);
-  // L.Routing.control({
-  //   waypoints: road.coordinates.map(coordinate => L.latLng(coordinate[1], coordinate[0])),
-  //   lineOptions: {
-  //     styles: [{color: road.color, opacity: 1, weight: 5}],
-  //     addWaypoints: false,
-  //     extendToWaypoints: false,
-  //     missingRouteTolerance: 0
-  //   },
-  //   addWaypoints: false,
-  // }).addTo(map);
+  map = L.map("map");
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  
+  map.setView([47.21818239970725, -1.5520947541977976], 12);
 });
 
-map.setView([47.21818239970725, -1.5520947541977976], 12);
+document.getElementById("submit")!.addEventListener("click", submit);
 
-main().then((model: any) => {
-  console.log("model: " + model);
+function submit() {
+  if (!isLoading()) {
+    const date = document.getElementById("date") as HTMLInputElement;
+    const time = document.getElementById("time") as HTMLInputElement;
+    const dateValue = date.value;
+    const timeValue = time.value;
+    if (dateValue == "" || timeValue == "") {
+      alert("Veuillez renseigner une date et une heure");
+      return;
+    }
+    console.log("date: " + dateValue + " time: " + timeValue);
+    const dateSplit = dateValue.split("-");
+    const timeSplit = timeValue.split(":");
+    const year = parseInt(dateSplit[0]);
+    const month = parseInt(dateSplit[1]) - 1;
+    const day = parseInt(dateSplit[2]);
+    const hours = parseInt(timeSplit[0]);
+    const minutes = parseInt(timeSplit[1]);
+    console.log("year: " + year + " month: " + month + " day: " + day + " hours: " + hours + " minutes: " + minutes);
+    const dateObject = new Date(year, month, day, hours, minutes);
+    console.log("dateObject: " + dateObject);
+    setData(dateObject);
+  } else {
+    alert("Veuillez patienter pendant le chargement des données");
+  }
+}
+
+function isLoading(): boolean {
+  return document.getElementById("loader") != null;
+}
+
+function setData(date: Date) {
   fetch("nantes_json/244400404_fluidite-axes-routiers-nantes-metropole.json").then(response => response.json()).then((data: any[]) => {
     data.forEach((dataPoint) => {
       const id = dataPoint.fields.cha_id;
@@ -133,7 +63,7 @@ main().then((model: any) => {
       // console.log("data id: " + id + " with name " + dataPoint.fields.cha_lib);
       const roadData = {
         "id": id,
-        "timestamp": convertDateToTimestamp(new Date(Date.now())),
+        "timestamp": convertDateToTimestamp(date),
       }
       predictForRoute(model, roadData).then((traffic: any) => {
         console.log("traffic: " + traffic);
@@ -144,16 +74,7 @@ main().then((model: any) => {
       });
     });
   });
-});
-
-window.addEventListener("unhandledrejection", (event) => {
-  console.error("Unhandled Promise Rejection:", event.reason);
-});
-
-window.addEventListener("error", (event) => {
-  console.error("Error:", event.error);
-});
-
+}
 
 function convertDateToTimestamp(date: Date): string {
 
